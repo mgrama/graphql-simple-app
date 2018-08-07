c6d56f76176828076ed900192690af9e9e339c10
Mark Grama <mark@grama.ru>
2018-08-07 17:01:02 +0300

    continue configuring project

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<ol>
-					{arr.map((item, index) => (
-						<ListItem
-							key={item.id}
-							number={index}
-							name={item.name}
-							century={item.century}
-							ekzegetType={item.ekzeget_type.title}
-						/>
-					))}
-				</ol>
+				Hello!
 			</div>
 		);
 	}
