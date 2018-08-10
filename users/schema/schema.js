8142cd706e4485e3c10b84ff9ed53ff2b5763d0f
Mark Grama <mark@grama.ru>
2018-08-10 10:05:38 +0300

    continue with schema

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,4 +1,28 @@
 const graphql = require('graphql');
 const {
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt
 } = graphql
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { GraphQLInt }
+    }
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+
+            }
+        }
+    }
+});
